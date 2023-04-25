d0d4020d88530f15261e1f80864c799e4919d598
lakpa sherpa <llakpa650@gmail.com>
2023-04-25 11:42:23 +0530

    lakpa python

--- lakpavs.py
+++
@@ -1,9 +0,0 @@
-height = float(input('enter the length'))
-width = float(input('enter the width'))
-area = height * width
-print('the area of rectangle is',area)
-pi=3.14
-radius=float(input('enter the radius'))
-height=float(input('enter the height'))
-volume = pi*(radius**2)*height
-print('the volume of cyinder is',volume)
